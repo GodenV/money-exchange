8ec8e213e7be424a6c21ef77ffe7b8001dc32e87
Vadzim Kersnovskiy <kersnachv@mail.ru>
2019-03-24 18:02:33 +0300

    Ready task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency > 10000)
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if(currency < 0)
+    return {};
+    vocabulary = {
+    }    
+    vocabulary["H"] = (currency - currency%50)/50;
+    currency = currency - vocabulary["H"]*50;
+    vocabulary["Q"] = (currency - currency%25)/25;
+    currency = currency - vocabulary["Q"]*25;
+    vocabulary["D"] = (currency - currency%10)/10;
+    currency = currency - vocabulary["D"]*10;
+    vocabulary["N"] = (currency - currency%5)/5;
+    currency = currency - vocabulary["N"]*5;
+    vocabulary["P"] = currency;
+    for(var key in vocabulary)
+    {
+        if(vocabulary[key] == 0)
+        delete vocabulary[key];
+    }
+    return vocabulary;
 }
